e64a6765f530342b5c8a5bd519852d98db9d501f
Farheen Mahmud <farheen.mahmud@my.stjohns.edu>
2025-05-06 06:47:04 -0400

    add

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,18 +3,29 @@ const fs = require('fs');
 const path = require('path');
 const usersPath = path.join(__dirname, '../data/users.json');
 
+function safeReadJSON(filePath) {
+  try {
+    const data = fs.readFileSync(filePath, 'utf8');
+    return data ? JSON.parse(data) : [];
+  } catch (err) {
+    return [];
+  }
+}
+
 exports.getRegister = (req, res) => {
   res.render('register');
 };
 
 exports.postRegister = (req, res) => {
   const { username, password } = req.body;
-  const users = JSON.parse(fs.readFileSync(usersPath));
+  const users = safeReadJSON(usersPath);
+
   if (users.find(user => user.username === username)) {
     return res.status(400).send('User already exists');
   }
+
   users.push({ username, password });
-  fs.writeFileSync(usersPath, JSON.stringify(users));
+  fs.writeFileSync(usersPath, JSON.stringify(users, null, 2));
   req.session.username = username;
   res.redirect('/video/dashboard/all');
 };
@@ -25,9 +36,11 @@ exports.getLogin = (req, res) => {
 
 exports.postLogin = (req, res) => {
   const { username, password } = req.body;
-  const users = JSON.parse(fs.readFileSync(usersPath));
+  const users = safeReadJSON(usersPath);
+
   const user = users.find(u => u.username === username && u.password === password);
   if (!user) return res.status(401).send('Invalid credentials');
+
   req.session.username = username;
   res.redirect('/video/dashboard/all');
 };
